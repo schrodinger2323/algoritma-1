9e5928dbf2ec2ec3ee72faabca4128b2a45d365c
schrodinger2323 <152845558+schrodinger2323@users.noreply.github.com>
2024-01-28 20:40:34 +0300

    Add files via upload

---
+++ JavaApplication5.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author asus
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //int sayi=21;
+    //if (sayi<20){
+        //System.out.println("sayi 20den kucuk");
+    //}
+    //else if(sayi<45){
+        //System.out.println("sayi 45den kucuk");
+    //}
+    int sayi=15;
+    if (sayi<17){
+        System.out.println("sayi 17den kucuk");
+    }
+    if (sayi<23){
+        System.out.println("sayi 23den de kucuk");
+    }
+
+    }
+    
+    
+}
